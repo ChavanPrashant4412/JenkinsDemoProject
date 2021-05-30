05b0200bc247b910d4de74673f05a24aa67796c1
ChavanPrashant4412 <automationengg.prashant07@gmail.com>
2021-05-30 10:15:44 +0530

    from source tree

--- src/test/java/testcases/DemoTest.java
+++ src/test/java/testcases/DemoTest.java
@@ -4,43 +4,47 @@ import org.testng.annotations.Test;
 
 import com.deskera.basePage.BasePage;
 
-public class DemoTest extends BasePage{
+public class DemoTest extends BasePage {
 
+	//Jenkins parameterrized Build test
+	// -Dbrowser="$browser" -Dgroups=$groups" -Denv="$env"
 	
-	@Test(groups={"Sanity"})
-	public void luanchBrowser(){
-		
+	@Test(groups = { "Sanity" })
+	public void luanchBrowser() {
+
 		System.out.println("Inside luanchBrowser");
 		String nameOfbrowser = System.getProperty("browser");
-		
-		System.out.println("Browser Name is ::-"+nameOfbrowser);
-		if(nameOfbrowser.equals("Chrome")){
+
+		System.out.println("Browser Name is ::-" + nameOfbrowser);
+		if (nameOfbrowser.equals("Chrome")) {
 			System.out.println("Opening in chrome browser");
-		}else{
+		} else {
 			System.out.println("Opening in Firefox browser");
 		}
 	}
-	
-	@Test(groups={"Sanity"})
-	public void printEnvironment(){
-		/*String envName=System.getProperty("env");
-		System.out.println("Environament Name is "+envName);*/
-		String url=globalProperties.getProperty(System.getProperty("env"));
-		System.out.println("Print the URL"+url);
+
+	@Test(groups = { "Sanity" })
+	public void printEnvironment() {
+		/*
+		 * String envName=System.getProperty("env");
+		 * System.out.println("Environament Name is "+envName);
+		 */
+		String url = globalProperties.getProperty(System.getProperty("env"));
+		System.out.println("Print the URL" + url);
 	}
-	
-	@Test(groups={"Smoke"})
-	public void m1(){
+
+	@Test(groups = { "Smoke" })
+	public void m1() {
 		System.out.println("M1");
 	}
-	
-	@Test(groups={"Regression"})
-	public void m2(){
+
+	@Test(groups = { "Regression" })
+	public void m2() {
 		System.out.println("M2");
 	}
-	
-	@Test(groups={"Sanity1"})
-	public void m3(){
-		System.out.println("Executed Sanity Test Cases");
+
+	@Test(groups = { "DryRun" })
+	public void m3() {
+		System.out.println("Executed DryRun Cases");
 	}
 }
